961f7a7a14260f37bba1ab5360cc9ebe374c213f
Huy Thanh <tathuythanh-vn@gmail.com>
2024-09-25 13:45:22 +0700

    add . gitignore

--- server/server.js
+++ server/server.js
@@ -2,14 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const PORT = process.env.PORT || 8080
+
 const corsOption = {
     origin: ["http://localhost:5173"],
 };
 
+//middleware
 app.use(cors(corsOption));
 
-const PORT = process.env.PORT || 8080
+app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }))
+
+
+
+
+//api call
 app.get("/api", (req, res) => {
     res.json("Hello from /api")
 })
