4181a3bcec1f1695b4a94b4a95c201a1bb55b1bb
Huy Thanh <tathuythanh-vn@gmail.com>
2024-11-18 17:25:48 +0700

    setup

---
+++ client/src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ErrorPage(props) {
+    return (
+        <div>
+            <h1>Error, the URL does not exist</h1>
+        </div>
+    );
+}
+
+export default ErrorPage;
---
+++ client/src/components/SignInPage/SignInPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SignInPage(props) {
+    return (
+        <div>
+            <h1>Sign In Pages</h1>
+        </div>
+    );
+}
+
+export default SignInPage;
