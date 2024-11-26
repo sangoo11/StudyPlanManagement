a009c9c54f2a437f1e9e08643f4169792ed81f4c
Ngo Tran Quang Sang <112809865+sangoo11@users.noreply.github.com>
2024-11-26 16:13:13 +0700

    Error Student Page

--- client/src/layouts/StudentPageLayout.jsx
+++ client/src/layouts/StudentPageLayout.jsx
@@ -1,9 +1,81 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router';
 
 function StudentPageLayout(props) {
+
+    const appName = 'EduOrganizer';
+    const appMail = 'eduorganizer@gmail.com';
+    const studentName = 'Student Name';
+    const title1 = 'Home Page';
+    const title2 = 'Learning Results';
+    const title3 = 'Learning Outcomes';
+
+    const navLinkStyles = ({ isActive }) => {
+        return {
+            color: isActive ? "#1DA599" : "black",
+            fontWeight: 400,
+            textDecoration: "none",
+            borderBottom: isActive ? "1px solid #1DA599" : "none",
+            paddingBottom: isActive ? "5px" : "0",
+        };
+    };
+
+
     return (
         <div>
+            <div className="fixed top-0 right-0 left-0 min-h-[10vh] bg-white items-center justify-center mt-4">
+                <div className="flex justify-center items-center space-x-12">
+                    <div className='fixed left-0'>
+                        <h1 className="text-3xl text-[#1DA599] font-bold pl-10">{appName}</h1>
+                    </div>
 
+                    <NavLink style={navLinkStyles} to="/student">{title1}</NavLink>
+                    <NavLink style={navLinkStyles} to="/student/learningresults">{title2}</NavLink>
+                    <NavLink style={navLinkStyles} to="/student/learningoutcome">{title3}</NavLink>
+
+                    <div className='fixed right-0'>
+                        <h1 className="text-3xl text-[#1DA599] font-bold pl-10">{studentName}</h1>
+                    </div>
+                </div>
+            </div>
+            <div className='fixed bottom-0 right-0 left-0 min-h-[10vh] bg-[#1DA599] px-10 py-6'>
+                <div className='grid grid-cols-[2fr_1fr_1fr_1fr_1fr] h-full min-h-[10vh] text-white'>
+                    <div className='w-4/6'>
+                        <h3 className='text-xl font-semibold mb-2'>{appName}</h3>
+                    </div>
+                    <div>
+                        <h3 className='text-xl font-semibold mb-2'>Company</h3>
+                        <ul className='text-xs font-thin space-y-2'>
+                            <li>About Us</li>
+                            <li>Services</li>
+                            <li>Community</li>
+                            <li>Testimorial</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className='text-xl font-semibold mb-2'>Support</h3>
+                        <ul className='text-xs font-thin space-y-2'>
+                            <li>Help Center</li>
+                            <li>Feed Back</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className='text-xl font-semibold mb-2'>Links</h3>
+                        <ul className='text-xs font-thin space-y-2'>
+                            <li>Courses</li>
+                            <li>Become Teacher</li>
+                            <li>Services</li>
+                            <li>All in One</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className='text-xl font-semibold mb-2'>Contact Us</h3>
+                        <p className='text-xs font-thin'>{appMail}</p>
+                    </div>
+                </div>
+            </div>
+            <Outlet />
         </div>
     );
 }
