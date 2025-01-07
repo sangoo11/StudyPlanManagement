156523cebb18ecaa7bc84b07dc30d29282e8ae72
Huy Thanh <tathuythanh-vn@gmail.com>
2025-01-07 16:03:12 +0700

    student

--- server/services/student.service.js
+++ server/services/student.service.js
@@ -1,42 +1,85 @@
-// const User = require('../models/user.model');
-// const Subject = require('../models/subject.model');
-// const Enrollment = require('../models/enrollment.model');
-// const { sql, Op } = require('@sequelize/core');
+const Account = require('../models/account.model');
+const Subject = require('../models/subject.model');
+const Enrollment = require('../models/enrollment.model');
+const { sql, Op } = require('@sequelize/core');
+const Student = require('../models/student.model');
 
-// class StudentService {
-//     static getCreditLearn = async ({ studentId }) => {
-//         const student = await User.findOne({
-//             where: {
-//                 id: studentId,
-//                 role: 'student'
-//             }
-//         });
-//         if (!student) throw new Error('Student not found or student role is invalid');
+class StudentService {
+    // static getCreditLearn = async (studentID) => {
+    //     const student = await Student.findOne({
+    //         where: {
+    //             id: studentID,
+    //         },
+    //         attributes: ['credit']
+    //     });
+    //     if (!student) throw new Error('Student not found');
 
-//         const enrollments = await Enrollment.findAll({
-//             where: {
-//                 studentId: studentId,
-//                 status: 'completed'
-//             },
-//             attributes: ['courseId'],
-//         });
+    //     return {
+    //         studentID: studentID,
+    //         creditLearned: student.credit,
+    //     };
+    //     // const enrollments = await Enrollment.findAll({
+    //     //     where: {
+    //     //         studentID: studentID,
+    //     //         completed: true
+    //     //     },
+    //     //     attributes: ['courseID'],
+    //     // });
 
-//         const courseIds = enrollments.map(enrollment => enrollment.courseId)
+    //     // const courseIDs = enrollments.map(enrollment => enrollment.courseID)
 
-//         const subjects = await Subject.findAll({
-//             where: {
-//                 id: courseIds
-//             },
-//             attributes: ['credit']
-//         });
+    //     // const subjects = await Subject.findAll({
+    //     //     where: {
+    //     //         id: courseIDs
+    //     //     },
+    //     //     attributes: ['credit']
+    //     // });
 
-//         const totalCredits = subjects.reduce((accumulator, subject) => accumulator + subject.credit, 0);
+    //     // const totalCredits = subjects.reduce((accumulator, subject) => accumulator + subject.credit, 0);
 
-//         return {
-//             studentId: studentId,
-//             creditLearned: totalCredits,
-//         };
-//     }
-// }
+    //     // return {
+    //     //     studentID: studentID,
+    //     //     creditLearned: totalCredits,
+    //     // };
+    // }
+    static getStudentByID = async (studentID) => {
+        const student = await Student.findOne({
+            where: {
+                id: studentID,
+            }
+        });
+        if (!student) throw new Error('Student not found');
 
-// module.exports = StudentService;
+        return student;
+    }
+
+    static updateStudentByID = async (updateData, studentID) => {
+        const student = await Student.findOne({
+            where: {
+                id: studentID
+            }
+        });
+        if (!student) throw new Error('Student not found');
+
+        await student.update(updateData);
+
+        return student;
+    }
+
+    static deleteStudentByID = async (studentID) => {
+        const student = await Student.findOne({
+            where: {
+                id: studentID
+            }
+        });
+        if (!student) throw new Error('Student not found');
+
+        await student.update({
+            status: 'terminated'
+        });
+
+        return student;
+    }
+}
+
+module.exports = StudentService;
