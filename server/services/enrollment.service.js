9bb621a0666399d8eb8ae01e5bdc8083976b21ea
Huy Thanh <tathuythanh-vn@gmail.com>
2025-01-11 02:23:38 +0700

    enrollment get

--- server/services/enrollment.service.js
+++ server/services/enrollment.service.js
@@ -10,43 +10,68 @@ const { ROLE } = require('./access.service');
 class EnrollmentService {
     static getIncompleteEnrollment = async (studentID) => {
         if (!studentID) throw new Error('Student ID is required');
-        const student = await Student.findByPk(studentID);
-        if (!student) throw new Error('Student not found');
+        const currentStudent = await Student.findByPk(studentID);
+        if (!currentStudent) throw new Error('Student not found');
 
-        const subjectLists = [];
+        let incompleteSubjectLists = [];
 
-        const courseLists = await Enrollment.findAll({
+        const incompleteCourseLists = await Enrollment.findAll({
             where: {
                 studentID: studentID,
                 completed: false,
             },
+            attributes: ['courseID'],
         });
 
-        const courseID = courseLists.map(course => course.courseID);
-        for (let course of courseID) {
-            const courseData = await Course.findByPk(course);
-            const subjectData = await Subject.findByPk(courseData.subjectID);
-            subjectLists.push(subjectData);
+        for (let course of incompleteCourseLists) {
+            const incompleteCourse = await Course.findByPk(course.courseID);
+            const incompleteSubject = await Subject.findByPk(incompleteCourse.subjectID);
+            incompleteSubjectLists.push(incompleteSubject);
         }
 
-        console.log(JSON.stringify(subjectLists));
-
         return {
-            message: 'Incomplete enrollment',
+            studentID: studentID,
+            incompleteSubjectLists: incompleteSubjectLists,
         }
     }
 
+    static getCompleteEnrollment = async (studentID) => {
 
+        if (!studentID) throw new Error('Student ID is required');
+        const currentStudent = await Student.findByPk(studentID);
+        if (!currentStudent) throw new Error('Student not found');
+
+        let completeSubjectLists = [];
+
+        const completeCourseLists = await Enrollment.findAll({
+            where: {
+                studentID: studentID,
+                completed: true,
+            },
+            attributes: ['courseID'],
+        });
+
+        for (let course of completeCourseLists) {
+            const completeCourse = await Course.findByPk(course.courseID);
+            const completeSubject = await Subject.findByPk(completeCourse.subjectID);
+            completeSubjectLists.push(completeSubject);
+        }
+
+        return {
+            studentID: studentID,
+            completeSubjectLists: completeSubjectLists,
+        }
+    }
 
     static enrollStudentInCourse = async (data, courseID) => {
         const transaction = await sequelize.transaction();
 
         if (!data || data.length === 0 || !courseID) throw new Error('Student ID and Course ID are required');
 
-        const course = await Course.findByPk(courseID);
-        if (!course) throw new Error('Course not found');
+        const currentCourse = await Course.findByPk(courseID);
+        if (!currentCourse) throw new Error('Course not found');
 
-        const subject = await Subject.findByPk(course.subjectID);
+        const subject = await Subject.findByPk(currentCourse.subjectID);
         const subjectCredit = subject.credit;
 
         const studentListsID = [];
@@ -54,19 +79,26 @@ class EnrollmentService {
         // Handle each student in the list
         try {
             for (let student of data.studentLists) {
+                // student model
                 const currentStudent = await Student.findByPk(student.studentID);
-                if (!currentStudent) throw new Error('Student not found');
+                if (!currentStudent) throw new Error(`Student with ID ${student.studentID} not found`);
 
-                const existingEnrollment = await Enrollment.findOne({
+                // get courseID of incomplete course
+                const studentIncompleteCourse = await Enrollment.findAll({
                     where: {
                         studentID: student.studentID,
-                        courseID: courseID,
                         completed: false,
-                    }
+                    },
+                    attributes: ['courseID'],
                 });
-                if (existingEnrollment) throw new Error('Student already enrolled in course');
 
-
+                // check if the student has an incomplete subject of course
+                for (let course of studentIncompleteCourse) {
+                    const existingCourse = await Course.findByPk(course.courseID);
+                    if (existingCourse.subjectID === currentCourse.subjectID) {
+                        throw new Error(`Student with ID ${student.studentID} already has an incomplete course of this subject`);
+                    }
+                }
 
                 await Enrollment.create({
                     studentID: student.studentID,
@@ -82,8 +114,8 @@ class EnrollmentService {
                     transaction: transaction,
                 });
 
-                await course.update({
-                    studentCount: course.studentCount + 1,
+                await currentCourse.update({
+                    studentCount: currentCourse.studentCount + 1,
                 }, {
                     transaction: transaction,
                 });
@@ -98,7 +130,7 @@ class EnrollmentService {
         return {
             credit: subjectCredit,
             courseID: courseID,
-            studentCount: course.studentCount,
+            studentCount: currentCourse.studentCount,
             studentLists: studentListsID,
             enrollmentDate: new Date(),
         }
